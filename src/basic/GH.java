439fcaded28ae8fe7c6466da31d8c05104f58a32
sun <niketjssun31@gmail.com>
2022-01-20 00:31:42 +0900

    good

---
+++ src/basic/GH.java
@@ -0,0 +1,35 @@
+package basic;
+public class GH {
+
+	public static void main(String[] args) {
+		
+		System.out.println("다이아몬드 층");
+		
+		int num=3;
+		
+		
+		for(int i=0; i<=num;i++) {
+			for(int k=num-i; k>0;k--)
+				System.out.print(" ");
+			
+			for(int j=0;j<=i*2;j++) {
+				System.out.print("*");
+				
+			}
+			
+			System.out.println();
+		}
+		
+		for(int i=0; i<num;i++) {
+			for(int k=0; k<=i;k++)
+				System.out.print(" ");	
+		
+			for(int j=num+1;j>=i*2;j--) 
+				System.out.print("*");
+			System.out.println();
+		}
+		
+		
+	}
+		
+}
